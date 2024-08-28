ecb49d97871ad7bb27780819a9da52f07ce4eec6
Vince Linise <vincelinise@gmail.com>
2024-08-28 22:33:37 +0200

    chore: Update svelte.config.js and add CNAME and robots.txt files

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,10 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: "index.html",
+			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
