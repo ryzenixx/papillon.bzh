30fe47698ffd6fd2942aeb361a0cbb30735467b6
godetremy <godetremy@free.fr>
2025-07-13 18:00:15 +0200

    fix: Build error with ssr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Hero from '../components/Hero.svelte';
 	import FeaturesSection from '../components/sections/FeaturesSection.svelte';
 	import IconsSection from '../components/sections/IconsSection.svelte';
-	import TrailerSection from '../components/sections/TrailerSection.svelte';
 	import TrySection from '../components/sections/TrySection.svelte';
 	import Footer from '../components/Footer.svelte';
 	import SecureSection from '../components/sections/SecureSection.svelte';
