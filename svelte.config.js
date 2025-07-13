f7549caa9ce3a2b58606a2e522670704c0565978
godetremy <godetremy@free.fr>
2025-07-13 18:11:14 +0200

    fix: blog is not prerendable (how it works with vince but not with me ahhhhhhh)

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,20 @@ export default {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		prerender: {
+			entries: [
+				'/',
+				'/blog',
+				'/blog/communique-10-06-2025',
+				'/donate',
+				'/download',
+				'/github/link/already',
+				'/github/link/error',
+				'/github/link/not_available',
+				'/github/link/success',
+				'/kenavo',
+			],
+		},
 	}
 };
